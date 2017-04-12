2d0a40a9f85c7b45329b7f93d818d9f11615de8b
RanaMohamed <r.mohamed@stud.fci-cu.edu.eg>
2017-04-12 18:22:56 +0200

    Sign-up

--- Smart-Home/src/DataAccess/Try2.java
+++ Smart-Home/src/DataAccess/Try2.java
@@ -1,13 +1,14 @@
 package DataAccess;
 
-import RestServices.SignIn;
+import Controller.User;
 
 public class Try2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		SignIn si = new SignIn();
-		System.out.println(si.getSignIn("1", "1"));
+		UserDBAccess udb = new UserDBAccess();
+		User u = new User("2", "2");
+		udb.SignUp(u);
 
 	}
 }
--- Smart-Home/src/DataAccess/UserDBAccess.java
+++ Smart-Home/src/DataAccess/UserDBAccess.java
@@ -9,23 +9,66 @@ import Controller.User;
 public class UserDBAccess {
 	static Connection currentCon;
 	static ResultSet rs = null;
-	
-	public void SignUp()
-	{
-		
+
+	public boolean SignUp(User u) {
+		Statement stmt = null;
+		String Query = "select * from Users where UserName= \"" + u.name + "\"";
+
+		try {
+			currentCon = ConnectionManager.getConnection();
+			stmt = currentCon.createStatement();
+			rs = stmt.executeQuery(Query);
+			boolean more = rs.next();
+
+			if (!more) {
+				Query = "insert into Users (UserName , UserPassword) values (\"" + u.name + "\" , \"" + u.password
+						+ "\" )";
+				stmt.executeUpdate(Query);
+			}
+
+			else if (more) {
+				System.out.println("Username already Exist");
+				return false;
+			}
+		}
+
+		catch (Exception ex) {
+			System.out.println("Adding failed: An Exception has occurred! " + ex);
+		}
+
+		// some exception handling
+		try {
+			if (rs != null) {
+				rs.close();
+				rs = null;
+			}
+			if (stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if (currentCon != null) {
+				currentCon.close();
+				currentCon = null;
+			}
+		} catch (Exception e) {
+
+		}
+		return false;
+
 	}
-	public void RemoveUser()
-	{
-		
+
+	public void RemoveUser() {
+
 	}
-	public void EditUser()
-	{
-		
+
+	public void EditUser() {
+
 	}
-	
+
 	public boolean checkUser(String username, String password) {
 		Statement stmt = null;
-		String Query = "select * from Users where UserName= \"" + username + "\" and UserPassword = \"" + password + "\"";
+		String Query = "select * from Users where UserName= \"" + username + "\" and UserPassword = \"" + password
+				+ "\"";
 
 		try {
 			currentCon = ConnectionManager.getConnection();
@@ -66,11 +109,11 @@ public class UserDBAccess {
 		return false;
 
 	}
-	
-	public User getUser(String username, String password){
+
+	public User getUser(String username, String password) {
 		User user = new User(username, password);
 		return user;
-		
+
 	}
 
 }
