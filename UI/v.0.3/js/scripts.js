4168d0520f5fef73b41b01f3e5b8715e66676375
desouky95 <tubesniper@gmail.com>
2017-04-14 17:42:10 +0200

    UI v.0.2 & v.0.3

---
+++ UI/v.0.3/js/scripts.js
@@ -0,0 +1,10 @@
+function addboard(nob) {
+    for (var i = 0; i < nob; i++) {
+        alert("OKKKK");
+    }
+}
+function alert() {
+
+        var h = getElementsByClassName('inter');
+        h.innerHTML = '';
+}
