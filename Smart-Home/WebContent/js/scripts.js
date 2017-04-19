5ecee8b54257eac09fc91d2c1d93183219ecd69a
husseinkk <husseinkk96@gmail.com>
2017-04-19 02:49:21 +0200

    css fixed again

--- Smart-Home/WebContent/js/scripts.js
+++ Smart-Home/WebContent/js/scripts.js
@@ -57,3 +57,34 @@ function themepopup(){
 function changeMain(color) {
 	getElementById("uppder").style.backgroundColor = "#" + color;
 }
+function popup() {
+	// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closebtn")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+}
+function changeMain() {
+	alert("Done");
+	document.getElementById('upper').style.backgroundColor ="red";
+}
