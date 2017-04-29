1610b7087c3ea00d85f4667d58dbf6a4db9013da
AbdallahAhmed <abdallah.se95@gmail.com>
2017-04-29 17:43:47 +0200

    checkDevice

--- Smart-Home/src/Controller/DeviceManagement.java
+++ Smart-Home/src/Controller/DeviceManagement.java
@@ -4,39 +4,45 @@ import DataAccess.DeviceDBAccess;
 
 public class DeviceManagement {
 	public Device device;
-	
-	public Boolean RegisterDevice(Device dev)
-	{
-		return (new DeviceDBAccess()).RegisterDevice(dev);
+
+	public Boolean RegisterDevice(Device dev) {
+		if (CheckDevice(dev)) {
+			return (new DeviceDBAccess()).RegisterDevice(dev);
+		} else
+			return false;
 	}
-	public Boolean RemoveDevice(Device dev)
-	{
+
+	public Boolean CheckDevice(Device dev) {
+		return (new DeviceDBAccess().CheckDevice(dev));
+	}
+
+	public Boolean RemoveDevice(Device dev) {
 		return true;
 	}
-	public Boolean EditDevice(Device dev)
-	{
+
+	public Boolean EditDevice(Device dev) {
 		return true;
 	}
-	public Device ViewDevice(Device dev)
-	{
+
+	public Device ViewDevice(Device dev) {
 		return dev;
 	}
-	public Boolean AddDevice()
-	{
+
+	public Boolean AddDevice() {
 		return true;
 	}
-	public Boolean SetStatus(int ID )
-	{
+
+	public Boolean SetStatus(int ID) {
 		return true;
 	}
-	public Device[] ViewDevices()
-	{
+
+	public Device[] ViewDevices() {
 		Device[] dev = null;
 		return dev;
 	}
-	public void ExecuteCommand(String s)
-	{
-		
+
+	public void ExecuteCommand(String s) {
+
 	}
 
 }
