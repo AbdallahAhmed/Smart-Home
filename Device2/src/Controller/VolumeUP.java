37d45356484cce3d34067efbec3e45745d8ce291
husseinkk <husseinkk96@gmail.com>
2017-05-04 00:02:13 +0200

    Client

---
+++ Device2/src/Controller/VolumeUP.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class VolumeUP extends Action{
+	public void execute() {
+		volume++;
+	}
+}
---
+++ Device2/src/Devices/TV.java
@@ -0,0 +1,63 @@
+package Devices;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.ws.rs.core.Form;
+
+import org.json.simple.parser.ParseException;
+
+import com.mysql.fabric.xmlrpc.base.Value;
+
+import Connectors.RestConnector;
+import Controller.Action;
+import Controller.Device;
+import Controller.Operation;
+import Controller.SocketConnector;
+
+public class TV {
+	public static void main(String[] args) throws ParseException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		RestConnector rc = new RestConnector("RegisterDevice", "POST");
+		Device d = new Device();
+		d.Id = 55;
+		d.model = "ABC";
+		d.name = "Room TV";
+		Operation o = new Operation();
+		ArrayList<String> values = new ArrayList<String>();
+		values.add("Turn On:TurnON");
+		values.add("Turn Off:TurnOFF");
+		o.values = (ArrayList<String>) values.clone();
+		/*o.values.add("Turn On:TurnON");
+		o.values.add("Turn Off:TurnOFF");*/
+		o.name = "On/Off";
+		o.UIComponent = "OnAndOff";
+		o.UIComponentID = 1;
+		d.operations.add(o);
+		Operation o1 = new Operation();
+		ArrayList<String> values1 = new ArrayList<String>();
+		values.add("Volume up: volumeUP");
+		values.add("Volume down: VolumeDOWN");
+		o.values = (ArrayList<String>) values.clone();
+		/*o.values.add("Turn On:TurnON");
+		o.values.add("Turn Off:TurnOFF");*/
+		o.name = "Volume";
+		o.UIComponent = "UpAndDown";
+		o.UIComponentID = 1;
+		d.operations.add(o1);
+		d.status = "OFF";
+		rc.addParamPost("Device", d.toJson().toJSONString());
+		rc.addParamPost("Username","admin");
+		rc.addParamPost("port", "4444");
+		System.out.println(rc.getJSONObject());
+		Connectors.SocketConnector sc = new Connectors.SocketConnector(5555);
+		while(true){
+			String msg = sc.recv();
+			msg = "Controller." + msg;
+			//System.out.println(sc.recv());
+			Action ac = (Action)Class.forName(msg).newInstance();
+			ac.execute();
+		}
+		
+		
+	}
+}
