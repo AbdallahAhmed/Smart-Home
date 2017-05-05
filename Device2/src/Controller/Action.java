a9a2c2180747696f791d7b70a0927eb92700da9d
AbdallahAhmed <abdallah.se95@gmail.com>
2017-05-05 18:09:23 +0200

    Revert "Client"
    This reverts commit 37d45356484cce3d34067efbec3e45745d8ce291.

--- Device2/src/Controller/Action.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-public abstract class Action {
-	public static boolean status = false;
-	public static int volume = 50;
-	public void execute(){};
-}
--- Device2/src/Controller/VolumeUP.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-public class VolumeUP extends Action{
-	public void execute() {
-		volume++;
-	}
-}
